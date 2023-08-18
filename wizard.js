15f59789be0d86ef4937045c992457b19b4c10bc
SajeedAninda <sajaninda@gmail.com>
2023-08-18 16:09:13 +0600

    Commit

--- wizard.js
+++ wizard.js
@@ -5,11 +5,11 @@ function getValueFromField(id) {
 }
 // CALCULATION SHARED FUNCTION 
 function setAreaCalculation(objectName, objectArea) {
-   let calculationDiv = document.querySelector("#calculationDiv");
+    let calculationDiv = document.querySelector("#calculationDiv");
     let count = calculationDiv.childElementCount;
     let p = document.createElement('p');
     p.classList.add('my-4');
-    p.innerHTML = `${count + 1}. ${objectName} ${objectArea} cm<sup>2</sup> <button class="btn btn-sm btn-success">Convert</button>`;
+    p.innerHTML = `${count + 1}. ${objectName} <span class="meter">${objectArea}</span> <span class=cmSup>cm<sup>2</sup></span>`;
     calculationDiv.appendChild(p);
 }
 
@@ -22,5 +22,30 @@ document.querySelector("#triangle-btn").addEventListener("click", function () {
         return;
     }
     let triangleArea = 0.5 * triangleBase * triangleHeight;
-    setAreaCalculation("Triangle" , triangleArea);
+    setAreaCalculation("Triangle", triangleArea);
+});
+
+
+// RECTANGLE 
+document.querySelector("#rectangle-btn").addEventListener("click", function () {
+    let rectangleWidth = getValueFromField("#rectangle-width");
+    let rectangleLength = getValueFromField("#rectangle-length");
+    if (isNaN(rectangleWidth) || rectangleWidth <= 0 || isNaN(rectangleLength) || rectangleLength <= 0) {
+        alert("Please Input Valid Numbers for Width and Length");
+        return;
+    }
+    let rectangleArea = rectangleWidth * rectangleLength;
+    setAreaCalculation("Rectangle", rectangleArea);
+});
+
+// PARALLELOGRAM 
+document.querySelector("#paraBtn").addEventListener("click", function () {
+    let parallelogramBase = getValueFromField("#Parallelogram-Base");
+    let parallelogramHeight = getValueFromField("#Parallelogram-Height");
+    if (isNaN(parallelogramBase) || parallelogramBase <= 0 || isNaN(parallelogramHeight) || parallelogramHeight <= 0) {
+        alert("Please Input Valid Numbers for Base and Height");
+        return;
+    }
+    let paralellogramArea = parallelogramBase * parallelogramHeight;
+    setAreaCalculation("Parallelogram", paralellogramArea);
 });
